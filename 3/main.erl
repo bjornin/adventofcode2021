cf088d73af00989790688c75e438f268463a8766
Björn Ingeson <bjorn@avassa.io>
2021-12-04 19:15:30 +0000

    d3p2

--- 3/main.erl
+++ 3/main.erl
@@ -1,5 +1,5 @@
 -module(main).
--compile(export_all).
+-export([main/0, main/1]).
 
 main() ->
     main(["input.txt"]).
@@ -7,9 +7,9 @@ main() ->
 main([File]) ->
     {ok, Bin} = file:read_file(File),
     Val = parse(Bin),
-    io:format("part1: ~p~n", [engine_stats(Val)]).
-% io:format("part2: ~p~n",[travel_aim(Val)]),
-% erlang:halt(0).
+    io:format("part1: ~p~n", [engine_stats(Val)]),
+    io:format("part2: ~p~n", [air_stats(Val)]),
+    erlang:halt(0).
 
 parse(Bin) when is_binary(Bin) ->
     [X || X <- string:tokens(binary_to_list(Bin), "\n")].
@@ -35,3 +35,25 @@ sum_bits(L) -> sum_bits(L, [0,0,0,0,0,0,0,0,0,0,0,0]).
 sum_bits([], Acc) -> Acc;
 sum_bits([H|T], Acc) ->
     sum_bits(T, lists:zipwith(fun(X, Y) -> list_to_integer([X]) + Y end, H, Acc)).
+
+air_stats([]) -> 0;
+air_stats(L) ->
+    O = sum_air(oxygen, L),
+    C = sum_air(co2, L),
+    list_to_integer(O, 2) * list_to_integer(C, 2).
+
+sum_air(M, L) -> sum_air(M, 1, L).
+sum_air(_, _, [H]) -> H;
+sum_air(M, Pos, L) -> sum_air(M, Pos, L, [], []).
+
+sum_air(M, Pos, [], Zeros, Ones) ->
+    if length(Ones) >= length(Zeros) andalso M =:= oxygen -> sum_air(M, Pos + 1, Ones);
+        length(Ones) < length(Zeros) andalso M =:= oxygen -> sum_air(M, Pos + 1, Zeros);
+        length(Ones) >= length(Zeros) andalso M =:= co2 -> sum_air(M, Pos + 1, Zeros);
+        length(Ones) < length(Zeros) andalso M =:= co2 -> sum_air(M, Pos + 1, Ones)
+    end;
+sum_air(M, Pos, [H|T], Zeros, Ones) ->
+    case lists:nth(Pos, H) of
+        $0 -> sum_air(M, Pos, T, [H|Zeros], Ones);
+        $1 -> sum_air(M, Pos, T, Zeros, [H|Ones])
+    end.
